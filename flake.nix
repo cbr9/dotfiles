caec61b94531db29e6c8bacfb0a66faa06526a32
cbr9 <cabero96@protonmail.com>
2023-08-12 16:06:10 +0200

    switch pkgs from master to unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
