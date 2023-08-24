1c08a1c71b359adc8506d356eb1ca08d12d08d14
andres.caberobusto <andres.caberobusto@sony.com>
2023-08-24 16:52:46 +0200

    simplify ssh config

--- modules/home-manager/ssh/default.nix
+++ modules/home-manager/ssh/default.nix
@@ -5,26 +5,12 @@
   ...
 }: let
   enable1PasswordAgent = nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent;
-  _1passwordAgent = "${config.home.homeDirectory}/.1password/agent.sock";
 in {
-  # home.sessionVariables = lib.mkIf enable1PasswordAgent {
-  #   SSH_AUTH_SOCK = _1passwordAgent;
-  # };
   programs.ssh = {
     enable = true;
-    forwardAgent = true;
+    forwardAgent = enable1PasswordAgent;
     extraConfig = lib.mkIf enable1PasswordAgent ''
-      IdentityAgent ${_1passwordAgent}
+      IdentityAgent "${config.home.homeDirectory}/.1password/agent.sock"
     '';
-
-    matchBlocks = {
-      destc0strapp15 = {
-        hostname = "destc0strapp15";
-        user = "decabera";
-        forwardX11 = true;
-        forwardX11Trusted = true;
-        forwardAgent = true;
-      };
-    };
   };
 }
