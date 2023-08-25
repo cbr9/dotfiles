2c4eef15c7f80e69eaf127771bdadc0aaee10f36
cbr9 <cabero96@protonmail.com>
2023-08-25 17:56:34 +0200

    add yubico modules

--- hosts/naboo/configuration.nix
+++ hosts/naboo/configuration.nix
@@ -5,6 +5,16 @@
 
   config = {
     networking.vpn = ["tailscale" "mullvad"];
+
+    security.pam.yubico = {
+      enable = true;
+      debug = false;
+      mode = "challenge-response";
+      id = ["15582547" "15582531"];
+    };
+
+    services.pcscd.enable = true;
+
     sys = {
       users = ["cabero"];
       hardware = {
