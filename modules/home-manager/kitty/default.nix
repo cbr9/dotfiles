e05ea0bcc208debe3a41a3140aea87922e84a9d2
cbr9 <cabero96@protonmail.com>
2023-08-26 21:36:10 +0200

    use block cursor

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -6,8 +6,10 @@
 }: {
   programs.kitty = {
     enable = true;
-    font = {
-      size = lib.mkForce 11;
-    };
+    font.size = lib.mkForce 11;
+    shellIntegration.mode = "no-cursor";
+    extraConfig = ''
+      cursor_blink_interval 0
+    '';
   };
 }
