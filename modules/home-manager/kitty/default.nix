46945dfea2bff6033048a0b343c970a7236843bb
cbr9 <cabero96@protonmail.com>
2023-08-27 00:25:11 +0200

    add padding and transparency

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -9,6 +9,8 @@
     font.size = lib.mkForce 11;
     shellIntegration.mode = "no-cursor";
     extraConfig = ''
+      background_opacity 0.90
+      window_padding_width 5
       cursor_blink_interval 0
     '';
   };
