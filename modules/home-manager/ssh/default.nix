0f237ad85981115137ffe9aad210f21b723ab48f
andres.caberobusto <andres.caberobusto@sony.com>
2023-08-28 13:35:09 +0200

    add SSH_AUTH_SOCK

--- modules/home-manager/ssh/default.nix
+++ modules/home-manager/ssh/default.nix
@@ -4,19 +4,24 @@
   lib,
   ...
 }: let
-  enable1PasswordAgent = nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent;
+  _1passwordAgent = {
+    enable = nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent;
+    path = "${config.home.homeDirectory}/.1password/agent.sock";
+  };
 in {
+  home.sessionVariables = lib.mkIf _1passwordAgent.enable {
+    SSH_AUTH_SOCK = _1passwordAgent.path;
+  };
   programs.ssh = {
     enable = true;
-    forwardAgent = enable1PasswordAgent;
-    extraConfig = lib.mkIf enable1PasswordAgent ''
-      IdentityAgent "${config.home.homeDirectory}/.1password/agent.sock"
+    forwardAgent = _1passwordAgent.enable;
+    extraConfig = lib.mkIf _1passwordAgent.enable ''
+      IdentityAgent ${_1passwordAgent.path}
     '';
     matchBlocks = {
       destc0strapp15 = {
         hostname = "destc0strapp15";
         user = "decabera";
-        forwardX11 = true;
       };
     };
   };
