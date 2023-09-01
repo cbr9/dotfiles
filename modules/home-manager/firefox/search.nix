25dc66bb18b26642b28d24908c310cf9fae29e72
cbr9 <cabero96@protonmail.com>
2023-09-01 21:47:31 +0200

    remove nixos wiki engine and add wikipedia alias

--- modules/home-manager/firefox/search.nix
+++ modules/home-manager/firefox/search.nix
@@ -46,15 +46,7 @@
 
       "Cambridge German-English Dictionary" = {
         urls = [{template = "https://dictionary.cambridge.org/dictionary/german-english/{searchTerms}";}];
-        updateInterval = 24 * 60 * 60 * 1000; # every day
         definedAliases = ["@ger"];
-      };
-
-      "NixOS Wiki" = {
-        urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-        iconUpdateURL = "https://nixos.wiki/favicon.png";
-        updateInterval = 24 * 60 * 60 * 1000; # every day
-        definedAliases = ["@nw"];
       };
 
       "YouTube" = {
@@ -69,12 +61,12 @@
             ];
           }
         ];
-        updateInterval = 24 * 60 * 60 * 1000; # every day
         definedAliases = ["@you"];
       };
 
       "DuckDuckGo".metaData.alias = "@d";
       "Bing".metaData.hidden = true;
+      "Wikipedia (en)".metaData.alias = "@wiki";
       "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
     };
   };
