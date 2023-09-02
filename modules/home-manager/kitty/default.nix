7365edfd6bcc363b04cffa2372775722d587079c
cbr9 <cabero96@protonmail.com>
2023-09-02 10:09:40 +0200

    minor changes

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -3,33 +3,35 @@
   config,
   nixosConfig,
   ...
-}:
-with lib; {
-  stylix = {
-    opacity.terminal = 0.9;
-    polarity = "dark";
-    targets.kitty.variant256Colors = true;
-  };
-
-  programs.kitty = {
-    enable = nixosConfig != {};
-    font = {
-      size = mkForce 15;
+}: let
+  cfg = config.programs.kitty;
+in
+  with lib; {
+    stylix = {
+      opacity.terminal = 0.9;
+      polarity = "dark";
+      targets.kitty.variant256Colors = true;
     };
-    shellIntegration.mode = "no-cursor";
-    settings = {
-      confirm_os_window_close = 0;
-      window_padding_width = 5;
-      cursor_blink_interval = 0;
-    };
-  };
 
-  xdg.configFile = mkIf config.programs.kitty.enable {
-    # Make kitty open file hyperlinks with xdg-open when clicking
-    # Since it doesn't seem to be the default behaviour
-    "kitty/open-actions.conf".text = ''
-      protocol file
-      action launch --type=os-window xdg-open $FILE_PATH
-    '';
-  };
-}
+    programs.kitty = {
+      enable = nixosConfig != {};
+      font = {
+        size = mkForce 15;
+      };
+      shellIntegration.mode = "no-cursor";
+      settings = {
+        confirm_os_window_close = 0;
+        window_padding_width = 5;
+        cursor_blink_interval = 0;
+      };
+    };
+
+    xdg.configFile = mkIf cfg.enable {
+      # Make kitty open file hyperlinks with xdg-open when clicking
+      # Since it doesn't seem to be the default behaviour
+      "kitty/open-actions.conf".text = ''
+        protocol file
+        action launch --type=os-window xdg-open $FILE_PATH
+      '';
+    };
+  }
