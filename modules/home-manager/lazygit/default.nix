7dc371dc3daa7cc3d087afb3c306618a963d0433
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-04 17:46:36 +0200

    add abbr

--- modules/home-manager/lazygit/default.nix
+++ modules/home-manager/lazygit/default.nix
@@ -1,6 +1,14 @@
-{...}: {
+{
+  lib,
+  config,
+  ...
+}:
+with lib; {
   programs.lazygit = {
     enable = true;
     settings = {os = {editPreset = "helix";};};
+  };
+  programs.fish.shellAbbrs = mkIf config.programs.lazygit.enable {
+    lg = "lazygit";
   };
 }
