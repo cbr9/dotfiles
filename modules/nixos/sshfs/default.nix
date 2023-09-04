c1678096b1e13a8544e5a31383d3744fcf651771
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-04 14:49:55 +0200

    allow writing several vpn connections

--- modules/nixos/sshfs/default.nix
+++ modules/nixos/sshfs/default.nix
@@ -4,39 +4,37 @@
   config,
   ...
 }:
-with lib; {
-  imports = [];
+with lib; let
+  profileOptions = types.submodule {
+    options = {
+      enable = mkEnableOption "Enable this profile";
+      user = mkOption {
+        type = types.nonEmptyStr;
+        description = "Your username on the remote server";
+      };
+      host = mkOption {
+        type = types.nonEmptyStr;
+        description = "The server from which to mount directories";
+      };
+      dirs = mkOption {
+        type = types.listOf types.nonEmptyStr;
+        description = "Folders that will be mounted locally using sshfs";
+      };
+      key = mkOption {
+        type = types.nonEmptyStr;
+        description = "SSH Key to use sshfs with";
+      };
+      mountpoint = mkOption {
+        type = types.nonEmptyStr;
+        default = "/mnt";
+      };
+    };
+  };
+in {
   options.services.sshfs = with lib; {
     profiles = mkOption {
       default = [];
-      type = types.listOf (
-        types.submodule
-        {
-          options = {
-            enable = mkEnableOption "Enable this profile";
-            user = mkOption {
-              type = types.nonEmptyStr;
-              description = "Your username on the remote server";
-            };
-            host = mkOption {
-              type = types.nonEmptyStr;
-              description = "The server from which to mount directories";
-            };
-            dirs = mkOption {
-              type = types.listOf types.nonEmptyStr;
-              description = "Folders that will be mounted locally using sshfs";
-            };
-            key = mkOption {
-              type = types.nonEmptyStr;
-              description = "SSH Key to use sshfs with";
-            };
-            mountpoint = mkOption {
-              type = types.nonEmptyStr;
-              default = "/mnt";
-            };
-          };
-        }
-      );
+      type = types.listOf profileOptions;
     };
   };
 
@@ -45,41 +43,37 @@ with lib; {
     mountProfile = profile: let
       uid = builtins.toString config.users.users.cabero.uid;
     in
-      if (profile.enable)
-      then
-        pkgs.lib.lists.forEach profile.dirs (dir: {
-          where = "${profile.mountpoint}/${dir}";
-          type = "fuse.sshfs";
-          what = "${profile.user}@${profile.host}:${dir}";
+      lib.optionals profile.enable (lib.forEach profile.dirs (dir: {
+        where = "${profile.mountpoint}/${dir}";
+        type = "fuse.sshfs";
+        what = "${profile.user}@${profile.host}:${dir}";
 
-          mountConfig = {
-            Options = pkgs.lib.strings.concatStringsSep "," [
-              "noauto"
-              "noatime"
-              "_netdev"
-              "reconnect"
-              "allow_other"
-              "uid=${uid}"
-              "x-systemd.automount"
-              "idmap=user"
-              "default_permissions"
-              "IdentityAgent=/home/cabero/.1password/agent.sock"
-            ];
-          };
-        })
-      else [];
+        mountConfig = {
+          Options = pkgs.lib.strings.concatStringsSep "," [
+            "noauto"
+            "noatime"
+            "_netdev"
+            "reconnect"
+            "allow_other"
+            "uid=${uid}"
+            "x-systemd.automount"
+            "idmap=user"
+            "default_permissions"
+            "IdentityAgent=/home/cabero/.1password/agent.sock"
+          ];
+        };
+      }));
 
     autoMountProfile = profile:
-      if profile.enable
-      then
-        lib.lists.forEach profile.dirs (dir: {
+      lib.optionals profile.enable (
+        lib.forEach profile.dirs (dir: {
           wantedBy = ["multi-user.target"];
           automountConfig = {
             TimeoutIdleSec = "60";
           };
           where = "${profile.mountpoint}/${dir}";
         })
-      else [];
+      );
   in
     lib.mkIf (config.services.sshfs.profiles != []) {
       environment.systemPackages = with pkgs; [sshfs];
