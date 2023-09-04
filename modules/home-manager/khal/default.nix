0fa4c9cee1a45554499025c794f1f38fcc83b296
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-04 11:27:08 +0200

    add gmail calendar (wip)

--- modules/home-manager/khal/default.nix
+++ modules/home-manager/khal/default.nix
@@ -1,5 +1,22 @@
 {...}: {
   programs.khal = {
     enable = true;
+    # locale = {};
+  };
+  accounts.calendar = {
+    basePath = ".calendar";
+    accounts.gmail = {
+      khal.enable = true;
+      primary = true;
+      primaryCollection = "gmail";
+      name = "Google Calendar";
+      local = {};
+      remote = {
+        # url = "https://calendar.google.com/";
+        type = "google_calendar";
+        userName = "cabero96@gmail.com";
+        passwordCommand = "op item get Google --vault Personal --fields app-password";
+      };
+    };
   };
 }
