f25ea7a0921f75fc7c64fb766b42b24d8b5cf775
cbr9 <cabero96@protonmail.com>
2023-09-06 00:42:58 +0200

    refresh kdeconnect on wm startup

--- modules/home-manager/awesome/rc.lua
+++ modules/home-manager/awesome/rc.lua
@@ -564,6 +564,8 @@ client.connect_signal("manage", function(c)
   end
 end)
 
+awful.spawn("kdeconnect-cli --refresh")
+
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
