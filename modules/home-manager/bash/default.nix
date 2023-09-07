c4483a0d48af90c7c49923034ab7098d5cc539b8
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-07 10:38:11 +0200

    disable bash

--- modules/home-manager/bash/default.nix
+++ modules/home-manager/bash/default.nix
@@ -2,7 +2,7 @@
   nixSh = "${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh";
 in {
   programs.bash = {
-    enable = true;
+    enable = false;
     profileExtra = ''
       if [[ -f ${nixSh} ]]; then
         source ${nixSh}
