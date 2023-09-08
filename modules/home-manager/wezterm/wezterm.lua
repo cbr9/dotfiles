c853b334fe1d22e34e64a274805fbd17298c044f
cbr9 <cabero96@protonmail.com>
2023-09-08 21:43:37 +0200

    refactor

--- modules/home-manager/wezterm/wezterm.lua
+++ modules/home-manager/wezterm/wezterm.lua
@@ -1,22 +1,23 @@
 local wezterm = require 'wezterm';
 wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
 
-local config = {
-  keys = {
-    { key = '=', mods = 'CTRL', action = wezterm.action.IncreaseFontSize },
-    { key = '_', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
-  },
-  tiling_desktop_environments = {
-    "X11 awesome"
-  },
-  color_scheme = 'Gruvbox dark, hard (base16)',
-  cursor_thickness = 2,
+local config = {}
 
-  dpi = 120,
-  enable_kitty_keyboard = true,
-  font_size = 15,
-  hide_tab_bar_if_only_one_tab = false,
-  font = wezterm.font('JetBrains Mono', { weight = 'Medium', italic = false }),
+config.keys = {
+  { key = '=', mods = 'CTRL', action = wezterm.action.IncreaseFontSize },
+  { key = '_', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
 }
+
+config.tiling_desktop_environments = {
+  "X11 awesome"
+}
+
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.cursor_thickness = 2
+config.dpi = 120
+config.enable_kitty_keyboard = true
+config.font_size = 15
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font('JetBrains Mono', { weight = 'Medium', italic = false })
 
 return config
