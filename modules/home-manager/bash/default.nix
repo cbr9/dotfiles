e3337ec132dd08b0034b83df853ce2e89d2c258e
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-08 14:03:47 +0200

    minor changes

--- modules/home-manager/bash/default.nix
+++ modules/home-manager/bash/default.nix
@@ -1,11 +1,11 @@
 {config, ...}: let
-  nixSh = "${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh";
+  nix = "${config.home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh";
 in {
   programs.bash = {
     enable = true;
     profileExtra = ''
-      if [[ -f ${nixSh} ]]; then
-        source ${nixSh}
+      if [[ -f ${nix} ]]; then
+        source ${nix}
         fish
       fi
     '';
