6d6bb17ab1464eba218ab32977e0f25ec9d1f263
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-11 14:19:09 +0200

    fix config paths

--- modules/home-manager/lf/default.nix
+++ modules/home-manager/lf/default.nix
@@ -288,7 +288,7 @@ in {
         mkShellCmd
         # bash
         ''
-          dirname -- "$fx" | head -c-1 | ${config.kitty.package}/bin/kitty +kitten clipboard
+          dirname -- "$fx" | head -c-1 | ${config.programs.kitty.package}/bin/kitty +kitten clipboard
         ''
       );
 
@@ -296,7 +296,7 @@ in {
         mkShellCmd
         # bash
         ''
-          printf '%s' "$fx" | ${config.kitty.package}/bin/kitty +kitten clipboard
+          printf '%s' "$fx" | ${config.programs.kitty.package}/bin/kitty +kitten clipboard
         ''
       );
 
@@ -304,7 +304,7 @@ in {
         mkShellCmd
         # bash
         ''
-          basename -a -- $fx | head -c-1 | ${config.kitty.package}/bin/kitty +kitten clipboard
+          basename -a -- $fx | head -c-1 | ${config.programs.kitty.package}/bin/kitty +kitten clipboard
         ''
       );
 
@@ -328,7 +328,7 @@ in {
                 print $0
             }' |
             if [ -n "$fs" ]; then cat; else tr -d '\n'; fi |
-            ${config.kitty.package}/bin/kitty +kitten clipboard
+            ${config.programs.kitty.package}/bin/kitty +kitten clipboard
         ''
       );
     };
