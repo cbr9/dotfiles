bbe95b4b121b29a912c799e2f8cdca300bb3b424
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-21 16:17:00 +0200

    dont make wezterm default terminal

--- modules/home-manager/wezterm/default.nix
+++ modules/home-manager/wezterm/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
   home.packages = [pkgs.wezterm];
-  home.sessionVariables.TERMINAL = "${pkgs.wezterm}/bin/wezterm";
+  # home.sessionVariables.TERMINAL = "${pkgs.wezterm}/bin/wezterm";
   xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
 }
