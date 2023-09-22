428b7f3e555973d0d7e65b33b223e5878e5e83a5
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-22 11:06:14 +0200

    fix wezterm problems: lf kitty image previews, helix opacity

--- modules/home-manager/wezterm/wezterm.lua
+++ modules/home-manager/wezterm/wezterm.lua
@@ -12,13 +12,26 @@ config.tiling_desktop_environments = {
   "X11 awesome"
 }
 
+config.enable_kitty_graphics = true
+config.set_environment_variables = {
+  KITTY_PID = "wezterm" -- hack to make ctpv load images with the kitty protocol in lf
+}
+
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.cursor_thickness = 2
 config.enable_kitty_keyboard = true
-config.enable_kitty_graphics = true
+config.term = "wezterm"
 config.font_size = 15
+config.window_padding = {
+  left = 10,
+  right = 0,
+  top = 0,
+  bottom = 0
+}
+config.enable_scroll_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.9
+config.text_background_opacity = 0.6
 config.font = wezterm.font('JetBrains Mono', { weight = 'Medium', italic = false })
 
 return config
