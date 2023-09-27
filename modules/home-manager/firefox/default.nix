4ed106a14296b6beb04803dbc295c12efe6ccc16
cbr9 <cabero96@protonmail.com>
2023-09-27 21:30:21 +0200

    add speech dispatcher to get rid of annoying firefox warning

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -17,6 +17,8 @@
     enable = nixosConfig != {};
     package = pkgs.firefox.override {
       cfg = {
+        speechSynthesisSupport = true;
+        enablePlasmaBrowserIntegration = true;
         enableGnomeExtensions = nixosConfig.services.xserver.desktopManager.gnome.enable;
         enableTridactylNative = builtins.elem nixosConfig.nur.repos.rycee.firefox-addons.tridactyl config.programs.firefox.profiles.default.extensions;
       };
