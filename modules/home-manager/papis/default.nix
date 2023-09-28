7d38899e789eb56b9d6f15ea3185714dc500b068
cbr9 <cabero96@protonmail.com>
2023-09-28 23:56:31 +0200

    add script to download pdfs from arxiv/acl

--- modules/home-manager/papis/default.nix
+++ modules/home-manager/papis/default.nix
@@ -3,8 +3,32 @@
   config,
   lib,
   ...
-}: {
-  programs.papis = {
-    enable = true;
+}: let
+  cfg = config.programs.papis;
+in {
+  options = with lib; {
+    programs.papis.package = mkOption {
+      type = types.package;
+      default = pkgs.papis;
+      defaultText = literalExpression "pkgs.papis";
+      description = "";
+    };
+  };
+  config = {
+    home.packages = [cfg.package];
+    programs.papis = {
+      enable = true;
+      settings = {
+        add-download-files = true;
+      };
+      libraries = {
+        papers = {
+          isDefault = true;
+          settings = {
+            dir = "${config.xdg.userDirs.documents}/Library/Papers";
+          };
+        };
+      };
+    };
   };
 }
