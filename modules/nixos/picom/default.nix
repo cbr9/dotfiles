40f839a8070db934252a3c44c559f993520443d8
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-28 12:01:56 +0200

    tweak picom

--- modules/nixos/picom/default.nix
+++ modules/nixos/picom/default.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  cfg = config.services.picom;
+in {
   config = {
     environment.systemPackages = with pkgs; [picom];
 
@@ -9,6 +15,13 @@
       shadow = true;
       backend = "xrender";
       vSync = true;
+      wintypes = {
+        popup_menu = {opacity = cfg.menuOpacity;};
+        dropdown_menu = {opacity = cfg.menuOpacity;};
+        normal = {
+          full-shadow = true;
+        };
+      };
     };
   };
 }
