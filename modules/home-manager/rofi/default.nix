4f48a154741acb759ef29b3d5e8a1ba0c96ad3b4
andres.caberobusto <andres.caberobusto@sony.com>
2023-09-28 13:55:01 +0200

    minor changes

--- modules/home-manager/rofi/default.nix
+++ modules/home-manager/rofi/default.nix
@@ -7,16 +7,16 @@
 with pkgs; let
   cfg = config.programs.rofi;
   keyboard_layout_selector = pkgs.writeScriptBin "rofi_switch_keyboard_layout" ''
-    ROFI_CMD="rofi -dmenu"
+    ROFI_CMD="${cfg.package}/bin/rofi -dmenu"
     KEYMAP_CACHE="${config.xdg.cacheHome}/keyboard-layout"
     LAYOUT_FILE="${config.xdg.configHome}/keyboard_layouts"
 
-    msg="Current Layout: "$(setxkbmap -query | grep layout | cut -d':' -f2 | sed 's/ //g')
+    msg="Current Layout: "$(${xorg.setxkbmap}/bin/setxkbmap -query | grep layout | cut -d':' -f2 | sed 's/ //g')
 
     selected=$(cat $LAYOUT_FILE | $ROFI_CMD -p "Keyboard Layout" -mesg "$msg" | awk '{print $1;}')
 
     if [ -n "$selected" ]; then
-        setxkbmap "$selected"
+        ${xorg.setxkbmap}/bin/setxkbmap "$selected"
         echo "$selected" > "$KEYMAP_CACHE"
     fi
   '';
