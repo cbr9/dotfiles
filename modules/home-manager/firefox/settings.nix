9efb5bb6f3d98723f91c84ed89872663e2a06804
cbr9 <cabero96@protonmail.com>
2023-09-29 21:25:43 +0200

    disable credit cards in firefox

--- modules/home-manager/firefox/settings.nix
+++ modules/home-manager/firefox/settings.nix
@@ -6,6 +6,8 @@
   programs.firefox.profiles.default.settings = (
     {
       "browser.fullscreen.autohide" = false;
+      "extensions.formautofill.creditCards.enabled" = false;
+      "extensions.formautofill.creditCards.available" = false;
     }
     // (lib.optionalAttrs config.xdg.configFile."tridactyl/tridactylrc".enable {
       "browser.startup.homepage" = "moz-extension://713d476b-6551-420c-b35f-87c66df5b4c6/static/newtab.html";
