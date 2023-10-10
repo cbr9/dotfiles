44971168201407eef5803390b4208f601beaf3d7
cbr9 <cabero96@protonmail.com>
2023-10-10 13:50:08 +0200

    fix potential version mismatch problem

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -42,7 +42,7 @@ with lib; {
       core.editor = "hx";
       init.defaultBranch = "main";
       gpg.format = "ssh";
-      gpg."ssh".program = lib.mkIf (nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent) "${pkgs._1password-gui}/bin/op-ssh-sign";
+      gpg."ssh".program = lib.mkIf (nixosConfig != {} && nixosConfig.programs._1password-gui.enable && nixosConfig.programs._1password-gui.sshAgent) "${nixosConfig.programs._1password-gui.package}/bin/op-ssh-sign";
       commit.gpgsign = true;
       user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOe0bugU6xBMHw8bIMlvEr9TnZ3S185UkTzRJUcmcW6v";
       push.autoSetupRemote = true;
