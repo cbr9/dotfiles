683aba01d665d916501624c875bfa7194aae8dd1
cbr9 <cabero96@protonmail.com>
2023-10-14 21:26:47 +0200

    minor changes

--- modules/nixos/nix/nixpkgs.nix
+++ modules/nixos/nix/nixpkgs.nix
@@ -2,27 +2,27 @@
   inputs,
   system,
   ...
-}: {
+}: let
+  config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      "electron-21.4.0"
+      "mailspring-1.11.0"
+    ];
+  };
+in {
   nixpkgs.pkgs = import inputs.nixpkgs {
-    inherit system;
+    inherit system config;
     overlays = [
       (import ../../../pkgs) # custom packages
       (final: prev: {
         typst = inputs.typst.packages.${system}.default;
         awesome = inputs.nixpkgs-f2k.packages.${system}.awesome-luajit-git;
         organize = inputs.organize.defaultPackage.${system};
-        stable = inputs.nixpkgs-stable.legacyPackages.${system};
-        master = inputs.nixpkgs-master.legacyPackages.${system};
+        stable = import inputs.nixpkgs-stable {inherit system config;};
+        master = import inputs.nixpkgs-master {inherit system config;};
       })
       inputs.helix.overlays.default
     ];
-
-    config = {
-      allowUnfree = true;
-      permittedInsecurePackages = [
-        "electron-21.4.0"
-        "mailspring-1.11.0"
-      ];
-    };
   };
 }
