9a7bbc0d87dfc18a0126641b2b09e57e098ba333
cbr9 <cabero96@protonmail.com>
2023-10-23 18:29:50 +0200

    change wallpaper

--- lib/default.nix
+++ lib/default.nix
@@ -64,8 +64,8 @@ in {
               stylix = {
                 base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
                 image = pkgs.fetchurl {
-                  url = "https://unsplash.com/photos/75UkcFlN2Xc/download?ixid=M3wxMjA3fDB8MXxhbGx8M3x8fHx8fDJ8fDE2OTc4MjQ4Mzh8&force=true";
-                  sha256 = "sha256-EIFzWp39cpuX1Cn1JvAXc+0A4+nJIWNkU4sgRWKXDwQ=";
+                  url = "https://unsplash.com/photos/dGMcpbzcq1I/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjk4MDc3NTI1fA&force=true";
+                  sha256 = "sha256-wVD+G1JRmDW/iDkejTtkkd3Ujs04oL+C5JSZNT5kdFo=";
                 };
               };
 
