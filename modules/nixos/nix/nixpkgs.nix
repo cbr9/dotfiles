b44127af48c2f077895ddfa485426b58bbf6bfe6
cbr9 <cabero96@protonmail.com>
2023-11-05 18:33:33 +0100

    use stable awesomewm

--- modules/nixos/nix/nixpkgs.nix
+++ modules/nixos/nix/nixpkgs.nix
@@ -20,7 +20,6 @@ in {
       (import ../../../pkgs) # custom packages
       (final: prev: {
         typst = inputs.typst.packages.${system}.default;
-        awesome = inputs.nixpkgs-f2k.packages.${system}.awesome-luajit-git;
         organize = inputs.organize.defaultPackage.${system};
         stable = import inputs.nixpkgs-stable {inherit system config;};
         master = import inputs.nixpkgs-master {inherit system config;};
