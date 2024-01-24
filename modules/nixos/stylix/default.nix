4d3ac014b797cf8232019cce594b458940ed7b0c
cbr9 <cabero96@protonmail.com>
2024-01-24 08:21:38 +0100

    change wallpaper

--- modules/nixos/stylix/default.nix
+++ modules/nixos/stylix/default.nix
@@ -6,8 +6,8 @@
   stylix = {
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     image = pkgs.fetchurl {
-      url = "https://unsplash.com/photos/aon-6I6Y6uQ/download?ixid=M3wxMjA3fDB8MXxhbGx8MjB8fHx8fHwyfHwxNzAwNjU2MjUwfA&force=true";
-      sha256 = "sha256-aFcIkyvS6Z5eCiHlt9mOym2TPJ3ddH7FK4zp7XRrt28=";
+      url = "https://unsplash.com/photos/CJAVJ5SF6gA/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzA2MDgwNzQxfA&force=true";
+      sha256 = "sha256-1XBFa+lpCecpVvriMVrisPTRH9KZg+asGbdNjtULx3A=";
     };
   };
   environment.variables = {
