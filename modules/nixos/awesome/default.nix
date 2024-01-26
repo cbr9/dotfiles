9a54b9e170157800ebe98cba7a3e75f8a4c8c093
cbr9 <cabero96@protonmail.com>
2024-01-26 13:01:21 +0100

    don't launch new dropbox instances on every awesomewm restart

--- modules/nixos/awesome/default.nix
+++ modules/nixos/awesome/default.nix
@@ -47,7 +47,10 @@ in {
           ${lib.optionalString config.programs._1password-gui.enable "1password --silent &"}
           ${lib.optionalString config.home-manager.users.cabero.services.betterlockscreen.enable "betterlockscreen -u ${config.stylix.image} &"}
           ${lib.optionalString config.programs.kdeconnect.enable "kdeconnect-cli --refresh &"}
-          ${pkgs.dropbox}/bin/dropbox &
+          if ! pgrep -x dropbox > /dev/null
+          then
+            ${pkgs.dropbox}/bin/dropbox &
+          fi
         '';
       };
     };
