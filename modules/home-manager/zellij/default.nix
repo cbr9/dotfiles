a396cb8380108b5257588fd2092202035e9af7ad
cbr9 <cabero96@protonmail.com>
2024-02-13 01:08:37 +0100

    build latest zellij version

--- modules/home-manager/zellij/default.nix
+++ modules/home-manager/zellij/default.nix
@@ -1,18 +1,15 @@
 {
   lib,
   config,
+  pkgs,
   ...
-}:
-with lib; let
+}: let
   cfg = config.programs.zellij;
   boolToString = bool:
     if bool
     then "true"
     else "false";
 in {
-  options.programs.zellij = {
-    allowOneInstance = mkEnableOption "Only allow one Zellij server at a time";
-  };
   config = {
     stylix.targets.zellij.enable = false;
     home.shellAliases = lib.mkIf cfg.enable {
@@ -35,7 +32,7 @@ in {
           theme "gruvbox-dark"
           ui {
           	pane_frames {
-          		hide_session_name ${boolToString cfg.allowOneInstance}
+          		hide_session_name ${boolToString false}
           	}
           }
         ''
@@ -43,15 +40,31 @@ in {
     };
 
     home.sessionVariables = lib.mkIf cfg.enable {
-      ZELLIJ_AUTO_ATTACH = boolToString cfg.allowOneInstance;
-      ZELLIJ_AUTO_EXIT = boolToString cfg.allowOneInstance;
+      ZELLIJ_AUTO_ATTACH = boolToString false;
+      ZELLIJ_AUTO_EXIT = boolToString false;
     };
     programs.zellij = {
       enable = true;
-      allowOneInstance = false;
-      enableZshIntegration = cfg.allowOneInstance;
-      enableBashIntegration = cfg.allowOneInstance;
-      enableFishIntegration = cfg.allowOneInstance;
+      package = pkgs.zellij.overrideAttrs (final: old: rec {
+        name = "zellij";
+        rev = "b677ffe75fb8e518441a0bd7df02abfb8dcc4989";
+        hash = "sha256-I0m/LSaDnFtsOZ4UUsvr/uMhNsbzvs3+RpnrcKqNrWw=";
+        depHash = "sha256-RGQe0fSnNYCDBhJ5pGbm7TPSoRa9x60NlGo3rgTJD6Y=";
+        src = pkgs.fetchFromGitHub {
+          inherit rev hash;
+          owner = "zellij-org";
+          repo = "zellij";
+        };
+        cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+          name = "${name}-vendor.tar.gz";
+          inherit src;
+          outputHash = depHash;
+        });
+      });
+
+      enableZshIntegration = false;
+      enableBashIntegration = false;
+      enableFishIntegration = false;
     };
   };
 }
