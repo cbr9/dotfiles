ffc4cffbcdff69d7efa28fcdffedde080e8e08ff
cbr9 <cabero96@protonmail.com>
2024-03-14 01:31:13 +0100

    automatically start spotifyd

--- modules/nixos/spotify/default.nix
+++ modules/nixos/spotify/default.nix
@@ -9,13 +9,16 @@
     allowedTCPPorts = [57621];
   };
 
+  age.secrets.spotify.file = ../../../secrets/spotify.age;
+
   home-manager.users.cabero = {
     services.spotifyd = {
       enable = true;
+      package = pkgs.stable.spotifyd;
       settings = {
         global = {
           username = "31vtbbqkijxmfhlidy4clznrf6a4";
-          password_cmd = "op item get Spotify --fields password";
+          password_cmd = "sudo cat /run/agenix/spotify";
           device_name = "${config.networking.hostName}-daemon";
           backend = "pulseaudio";
           bitrate = 320;
@@ -27,6 +30,6 @@
       spotify = pkgs.writeScriptBin "spotify" ''
         ${pkgs.spotify}/bin/spotify --disable-gpu # fix font glitch
       '';
-    in [pkgs.spotify-tui pkgs.spotify (lib.hiPrio spotify)];
+    in [pkgs.spotify (lib.hiPrio spotify)];
   };
 }
