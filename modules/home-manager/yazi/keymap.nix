c247ea698bbd5d67addb08fced8157416c30e9b6
cbr9 <cabero96@protonmail.com>
2024-03-19 09:36:30 +0100

    create a mkShellCmd function for yazi

--- modules/home-manager/yazi/keymap.nix
+++ modules/home-manager/yazi/keymap.nix
@@ -1,10 +1,36 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  mkShellCmd = {
+    confirm,
+    block,
+    script,
+  }: let
+    args = [
+      (lib.optionalString confirm "--confirm")
+      (lib.optionalString block "--block")
+    ];
+  in "shell ${lib.concatStringsSep " " args} '${script}'";
+in {
   programs.yazi.keymap = {
     manager = {
       prepend_keymap = [
         {
           on = ["<C-d>" "a"];
-          run = "shell --confirm '${pkgs.xdragon}/bin/dragon --all --and-exit --on-top ''$@'";
+          run = mkShellCmd {
+            confirm = true;
+            block = false;
+            script = (
+              # bash
+              ''
+                printf "%s\n" "$@" > /tmp/fx.tmp;
+                ${pkgs.xdragon}/bin/dragon --all --and-exit --on-top --stdin < /tmp/fx.tmp
+                rm /tmp/fx.tmp
+              ''
+            );
+          };
           desc = "Drag-and-drop all files at once";
         }
         {
