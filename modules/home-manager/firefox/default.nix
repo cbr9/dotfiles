b2dbecf8286a05df75c593df36c8783bf36f33d4
cbr9 <cabero96@protonmail.com>
2024-05-23 18:20:16 +0200

    disable firefox

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -17,7 +17,7 @@
   };
   home.packages = lib.mkIf config.programs.firefox.enable [pkgs.speechd];
   programs.firefox = {
-    enable = nixosConfig != {};
+    enable = false;
     package = pkgs.firefox.override {
       nativeMessagingHosts = (
         [pkgs.plasma-browser-integration]
