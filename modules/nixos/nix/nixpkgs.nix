3a35838298f5e0c3159e339522980b7f0032d3dc
cbr9 <cabero96@protonmail.com>
2024-07-12 10:40:46 +0200

    add yazi overlay

--- modules/nixos/nix/nixpkgs.nix
+++ modules/nixos/nix/nixpkgs.nix
@@ -8,21 +8,22 @@
     permittedInsecurePackages = [
       "electron-25.9.0"
       "zotero-6.0.27"
+      "electron-27.3.11"
     ];
   };
 in {
   nixpkgs.pkgs = import inputs.nixpkgs {
     inherit system config;
     overlays = [
+      inputs.helix.overlays.default
+      inputs.yazi.overlays.default
       (final: prev: {
-        typst = inputs.typst.packages.${system}.default;
         awesome = inputs.nixpkgs-f2k.packages.${system}.awesome-luajit-git;
         organize = inputs.organize.defaultPackage.${system};
         stable = import inputs.nixpkgs-stable {inherit system config;};
         master = import inputs.nixpkgs-master {inherit system config;};
         agenix = inputs.agenix.packages.x86_64-linux.default.override {ageBin = "${prev.age}/bin/age";};
       })
-      inputs.helix.overlays.default
     ];
   };
 }
