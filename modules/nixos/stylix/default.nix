9c9e94aa61b2f23661b34cab0caebd5729c78b3b
cbr9 <cabero96@protonmail.com>
2024-07-12 10:41:14 +0200

    change wallpaper, enable stylix

--- modules/nixos/stylix/default.nix
+++ modules/nixos/stylix/default.nix
@@ -4,10 +4,11 @@
   ...
 }: {
   stylix = {
+    enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     image = pkgs.fetchurl {
-      url = "https://unsplash.com/photos/AXLn5gBQeck/download?ixid=M3wxMjA3fDB8MXxhbGx8MXx8fHx8fDJ8fDE3MTA1ODgzODF8&force=true";
-      sha256 = "sha256-WMj2hLrOpVjXkqHfLpFG5GxBESS0bmg2XljJcTKch4U=";
+      url = "https://unsplash.com/photos/Hx8HaI4ERkA/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE5MTM4NDgyfA&force=true";
+      sha256 = "sha256-0K3ei7oDuW3LFl1UPDHWvLMSARug3olCmGiKno9ejXI=";
     };
   };
   environment.variables = {
