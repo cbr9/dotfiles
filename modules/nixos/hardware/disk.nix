761a2772cba2d85cf66f28a49c2aaaec08fa7aec
cbr9 <cabero96@protonmail.com>
2024-07-29 21:45:34 +0200

    remove unused code

--- modules/nixos/hardware/disk.nix
+++ modules/nixos/hardware/disk.nix
@@ -1,26 +1,9 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}:
-with pkgs;
-with lib; let
-  cfg = config.sys;
-in {
-  options.sys.hardware.usb = {
-    udisk2 = mkOption {
-      description = "Enable udisk for easy mounting an unmounting of usb drives";
-      type = types.bool;
-      default = true;
-    };
-  };
-
+{...}: {
   config = {
-    services.udisks2.enable = cfg.hardware.usb.udisk2;
-
-    # Pretty much all of my systems have ssd these days.
-    # So we should always enable trim.
+    services.udisks2.enable = true;
+    services.devmon.enable = true;
+    services.gvfs.enable = true;
+    # For SSDs
     services.fstrim.enable = true;
   };
 }
