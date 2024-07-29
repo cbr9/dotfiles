dbe101e948c423ed2e4b2c61c5ceceeb7eb2e088
cbr9 <cabero96@protonmail.com>
2024-07-29 21:45:18 +0200

    remove adwaita icon theme

--- modules/nixos/betterlockscreen/default.nix
+++ modules/nixos/betterlockscreen/default.nix
@@ -17,11 +17,6 @@
       };
     };
 
-    gtk.iconTheme = {
-      package = pkgs.gnome.adwaita-icon-theme;
-      name = "Adwaita";
-    };
-
     xdg.configFile = {
       "betterlockscreen/betterlockscreenrc" = {
         enable = config.home-manager.users.cabero.services.betterlockscreen.enable && config.programs.i3lock.u2fSupport;
