9b6dfaeb6d8b0af865e9e85cd71cf4783896d862
cbr9 <cabero96@protonmail.com>
2024-08-02 01:19:14 +0200

    change transmission pkg

--- modules/nixos/transmission/default.nix
+++ modules/nixos/transmission/default.nix
@@ -1,6 +1,7 @@
-{...}: {
+{pkgs, ...}: {
   services.transmission = rec {
     enable = true;
+    package = pkgs.transmission_4-gtk;
     user = "cabero";
     home = "/home/${user}";
     settings.watch-dir = "${home}/Downloads";
