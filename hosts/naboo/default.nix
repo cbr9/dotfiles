bb0a6cc04c23bc8927517e51a2d50fa7c293ad07
cbr9 <cabero96@protonmail.com>
2024-08-29 16:09:42 +0200

    remove mullvad

--- hosts/naboo/default.nix
+++ hosts/naboo/default.nix
@@ -1,12 +1,8 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./hardware-configuration.nix
     ./disks.nix
     ./openrgb.nix
     ./logitech.nix
   ];
-  services.mullvad-vpn = {
-    enable = true;
-    package = pkgs.mullvad-vpn;
-  };
 }
